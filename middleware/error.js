5b91c353b87704a311d62f3d03928e3d10de5054
amujuruprasad <prasadamujuru@gmail.com>
2021-01-04 11:18:28 +0530

    removed tg suppport

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,6 @@
 const winston = require("winston");
-const tgb = require("../helpers/telegram");
 
 module.exports = async function (err, req, res, next) {
-  await tgb.alert_Dev(err.message);
-  await tgb.alert_Dev(err.stack);
   winston.error(err.message, err);
   res.status(500).send("Something failed.");
 };
